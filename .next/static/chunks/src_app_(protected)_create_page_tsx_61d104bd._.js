fdd01ae1ade063a0855bb541cf8d63e27f165801
Ashuthosh0 <pvgar09@gmail.com>
2026-09-25 10:47:40 +0530

    Untrack .next directory

--- .next/static/chunks/src_app_(protected)_create_page_tsx_61d104bd._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_1b1b5a15._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_(protected)_layout_tsx_1b0167e3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_61700394._.js",
-  "static/chunks/node_modules_95e22b04._.js"
-],
-    source: "dynamic"
-});
